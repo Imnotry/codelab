e70ed2652c41e8c5c5977c189c0543a7e6243f49
unknown <apangnaufal30@gmail.com>
2024-03-25 06:45:21 +0700

    first commit

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Tabung tabung = new Tabung(" tabung");
         Kubus kubus = new Kubus(" kubus");
-        Balok balok = new Balok(" balok");
+        var balok = new Balok(" balok");
 
         balok.inputNilai();
         balok.luasPermukaan();
@@ -20,134 +20,6 @@ public class Main {
     }
 }
 
-import java.util.Scanner;
-class Balok extends BangunRuang{
-    Scanner scanner = new Scanner(System.in);
-    private double panjang;
-    private double lebar;
-    private double tinggi;
-
-    public Balok(String namee) {
-        super(namee);
-    }
-
-    public void inputNilai(){
-        super.inputNilai();
-        System.out.print("Input panjang: ");
-        panjang = scanner.nextDouble();
-        System.out.print("Input lebar: ");
-        lebar = scanner.nextDouble();
-        System.out.print("Input tinggi: ");
-        tinggi = scanner.nextDouble();
-    }
-
-    @Override
-    public void luasPermukaan(){
-        double hasil = 2 * ((panjang*lebar) + (panjang*tinggi) + (lebar*tinggi));
-        super.luasPermukaan();
-        System.out.println("Hasil luas permukaan: " + hasil);
-    }
-
-    @Override
-    public void volume(){
-        double hasil = panjang * lebar * tinggi;
-        super.volume();
-        System.out.println("Hasil volume: " + hasil);
-    }
-}
-
-import java.util.Scanner;
-class Kubus extends BangunRuang {
-    Scanner scanner = new Scanner(System.in);
-    private double sisi;
-
-    public Kubus(String name) {
-        super(name);
-    }
-
-    @Override
-    public void inputNilai(){
-        super.inputNilai();
-        System.out.print("Input sisi: ");
-        sisi = scanner.nextDouble();
-    }
-
-    @Override
-    public void luasPermukaan(){
-        double hasil = 6 * Math.pow(sisi, 2);
-        super.luasPermukaan();
-        System.out.println("Hasil luas permukaan: " + hasil);
-    }
-
-    @Override
-    public void volume(){
-        double hasil = Math.pow(sisi, 3);
-        super.volume();
-        System.out.println("Hasil volume: " + hasil);
-    }
-}
-
-import java.util.Scanner;
-class Tabung extends BangunRuang{
-    Scanner scanner = new Scanner(System.in);
-    private double tinggi;
-    private double jari_jari;
-
-    Tabung(String nameBangun) {
-        super(nameBangun);
-    }
-
-    @Override
-    public void inputNilai(){
-        super.inputNilai();
-        System.out.print("Input tinggi: ");
-        tinggi = scanner.nextDouble();
-        System.out.print("Input jari-jari: ");
-        jari_jari = scanner.nextDouble();
-    }
-
-    @Override
-    public void luasPermukaan(){
-        double hasil = 2 * Math.PI * jari_jari * (jari_jari + tinggi);
-        super.luasPermukaan();
-        System.out.println("Hasil luas permukaan: " + hasil);
-    }
 
 
-    @Override
-    public void volume(){
-        double hasil = Math.PI * Math.pow(jari_jari, 2) * tinggi;
-        super.volume();
-        System.out.println("Hasil volume: " + hasil);
-    }
-}
 
-import java.util.Scanner;
-class BangunRuang {
-    private String name;
-
-    BangunRuang(String namee){
-        this.name = namee;
-    }
-
-    public void inputNilai(){
-        System.out.println("Input nilai ");
-    }
-
-    public void luasPermukaan(){
-        System.out.println("\nMenghitung luas permukaan bangun" + name);
-    }
-
-    public void volume(){
-        System.out.println("Menghitung volume bangun" + name);
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-}
